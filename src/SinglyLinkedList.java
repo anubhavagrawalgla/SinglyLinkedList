764d3594da6f711e32617cf6d10e82303d807e8a
anubhavagrawalgla <anubhav.agrawal_ccv17@gla.ac.in>
2019-02-26 15:19:46 +0530

    singly linked list insert

--- src/SinglyLinkedList.java
+++ src/SinglyLinkedList.java
@@ -1,7 +1,9 @@
-package main;
+//package main;
+
+import java.util.ArrayList;
 
 public class SinglyLinkedList {
-    private class Node{
+    private class Node {
         private int data;
         private Node next;
 
@@ -20,5 +22,74 @@ public class SinglyLinkedList {
         public void setNext(Node next) {
             this.next = next;
         }
+
+        public Node(int data) {
+            this.data = data;
+        }
+
+        public Node(int data, Node next) {
+            this.next = next;
+            this.data = data;
+        }
+
     }
+
+    private Node head = null;
+    private int size = 0;
+
+    public static void main(String[] args) {
+        SinglyLinkedList linkedList = new SinglyLinkedList();
+        System.out.println(linkedList);
+        linkedList.insert(42);
+        linkedList.insert(62);
+        System.out.println(linkedList);
+
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder response = new StringBuilder();
+
+        response.append("[");
+
+        Node temp = this.head;
+        while (temp != null){
+            response.append(temp.getData());
+            if(temp.next != null){
+                response.append(" ==> ");
+            }
+            temp = temp.next;
+        }
+
+
+        response.append("]");
+
+        return response.toString();
+
+    }
+    private void insertHead(int data){
+        Node newNode = new Node(data, this.head);
+        this.head  = newNode;
+        size++;
+
+    }
+    private void insertAfter(int data, Node node){
+        Node newNode = new Node(data, node.next);
+        node.next = newNode;
+        size++;
+
+     }
+     public void insert(int data){
+        if(head == null){
+            insertHead(data);
+        }
+        else{
+            Node temp = this.head;
+            while (temp.next != null){
+                temp = temp.next;
+            }
+            insertAfter(data,temp);
+
+        }
+     }
 }
